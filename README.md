d185214b23355764d0a4d1a72af43182139805f6
munawr <munawart4tirur@gmail.com>
2024-10-25 12:06:21 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# novi_indus_machine_test
+
+A new Flutter machine test.
+
---
+++ android/app/src/main/kotlin/com/novi/ayurvedic/novi_indus_machine_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.novi.ayurvedic.novi_indus_machine_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
